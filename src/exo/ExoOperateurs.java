97ec5db5c07926d7d84e7bdf0661c673b7834f34
akimt <a.kimtsaris@outlook.com>
2020-10-07 16:30:49 +0200

    Matière vue:
    - variable
    - opération
    - type casting

--- src/exo/ExoOperateurs.java
+++ src/exo/ExoOperateurs.java
@@ -22,18 +22,7 @@ public class ExoOperateurs {
         System.out.printf("%d * %d = %d\n", a, b, (a*b));
         System.out.printf("%d / %d = %.1f\n", a, b, ((double)a/b));
         System.out.printf("%d DIV %d = %d\n", a, b, (a/b));
-
         System.out.printf("%d MOD %d = %d\n", a, b, (a%b));
-
-        // les marqueur
-
-        // int, short, byte, long = %d
-        // char = %c
-        // String = %s
-        // float, double = %f
-        // floating formaté : %.2f (2 chiffres après la virgule)
-
-        System.out.println(a + " MOD " + b + " = " + (a%b));
 
 
     }
