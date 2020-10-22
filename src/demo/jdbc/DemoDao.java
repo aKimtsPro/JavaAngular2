69ebcb55bb55e7d806c92fefc2e26d86ea4e266c
akimt <a.kimtsaris@outlook.com>
2020-10-22 17:09:08 +0200

    Matière vue:
    - variable
    - opération
    - type casting
    - structure conditionnelle if ... else if ... else

--- src/demo/jdbc/DemoDao.java
+++ src/demo/jdbc/DemoDao.java
@@ -1,16 +1,19 @@
 package demo.jdbc;
 
+import demo.jdbc.dao.SectionDAO;
 import demo.jdbc.dao.StudentDAO;
+import demo.jdbc.entity.Section;
 import demo.jdbc.entity.Student;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class DemoDao {
 
     public static void main(String[] args) {
 
+        // StudentDAO
         Student std = null;
-
         try (StudentDAO dao = new StudentDAO()){
             std = dao.getById(1000);
         }
@@ -23,6 +26,13 @@ public class DemoDao {
             (std == null) ?
                 "non trouvé" : std.getFirst_name() + " " + std.getLast_name()
         );
+
+        // SectionDAO
+
+        SectionDAO sDao = new SectionDAO();
+        List<Section> sectList = sDao.getAll();
+        System.out.println("nombre de section : "
+                + ((sectList == null) ? "vide" : sectList.size()));
 
     }
 }
