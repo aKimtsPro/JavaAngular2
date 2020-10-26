90d47292836002713884f4601289f11710ebed54
akimt <a.kimtsaris@outlook.com>
2020-10-26 12:43:43 +0100

    Matière vue:
    - variable
    - opération
    - type casting
    - structure conditionnelle if ... else if ... else

---
+++ src/demo/jdbc/DemoInjection.java
@@ -0,0 +1,29 @@
+package demo.jdbc;
+
+import demo.jdbc.dao.SectionDAO;
+import demo.jdbc.dao.SectionErrorException;
+import demo.jdbc.entity.Section;
+
+public class DemoInjection {
+
+    public static void main(String[] args) {
+
+        SectionDAO dao = new SectionDAO();
+
+        Section sect = new Section();
+        sect.setSection_id(0);
+        sect.setSection_name("injection sql' WHERE section_name LIKE 'c%'; DELETE FROM student; -- ");
+        sect.setDelegate_id(0);
+
+        boolean updated = false;
+        try {
+            updated = dao.update(sect);
+        } catch (SectionErrorException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("updated? : " + updated);
+
+    }
+
+}
